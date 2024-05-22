b5e3740d7782e8f7459292557f7a375d7212b38d
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-22 18:19:22 +0200

    added concurrently package & getting users from mongodb

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
-import './App.css';
+import Axios from "axios";
+import { useState, useEffect } from "react";
+// https://jsonplaceholder.typicode.com/users
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState([]);
+  
+  Axios.get("http://localhost:5000/users").then((res) => {
+    console.log(res.data);
+  });
+
+  /* const users = [
+    {
+      id: 1,
+      name: "Nour",
+      age: 20,
+      email: "nourhomsi@live.com",
+    },
+    {
+      id: 2,
+      name: "Majeg",
+      age: 25,
+      email: "majidhomsi@live.com",
+    },
+    {
+      id: 3,
+      name: "Anas",
+      age: 30,
+      email: "anashomsi@live.com",
+    },
+  ]; */
   return (
     <>
-    <h1>TEST</h1>
+      {/*  {users.map((user) => (
+        <ul>
+          <li>Name: {user.name}</li>
+          <li>Age: {user.age}</li>
+          <li>Email: {user.email}</li>
+        </ul>
+      ))} */}
     </>
   );
 }
-
-export default App;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000
@@ -10,6 +11,7 @@ connectDB();
 
 
 const app = express();
+app.use(cors());
 
 
 app.get("/", (req, res) => {
