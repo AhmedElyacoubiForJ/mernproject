ecf68555abdb5c7d81592690c78ecdb5e6764ad1
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-22 13:47:15 +0200

    testing

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log("Server Starting...")
+// console.log("Server Starting...")
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World, from node express server");
+});
+
+app.get("/users", (req, res) => {
+    res.send("<h1>users</h1>");
+});
+
+app.listen("3001", ()=> {
+    console.log("Server Running...");
+});
